75157239fe811b0b675793621fb0289930bb842a
Kali <kali@kali>
2023-11-21 14:32:50 -0500

    Forgot that if you name a single file to the add command it only adds that file

---
+++ PasswordGenerator.py
@@ -0,0 +1,19 @@
+import string
+import random
+
+length = int(input("Enter desired password length: "))
+
+character_list = "AbcdeFgHhIijJkKlLmMn1234567890!@#$%^&*()-_"
+
+password = []
+
+for i in range(length):
+    # Picking a random character from character list
+    random_char = random.choice(character_list)
+
+    # appending a random character to password
+    password.append(random_char)
+
+# printing password as a string
+print("".join(password))
+
